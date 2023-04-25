bad04687037ff5f41f1ea38d9406db6e8613b8d9
Karen <karen2005yulieth@gmail.com>
2023-04-25 15:27:24 -0500

    prueba

--- src/Servidor/Servicios/ServiceOperador.java
+++ src/Servidor/Servicios/ServiceOperador.java
@@ -9,7 +9,7 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-
+//
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 
